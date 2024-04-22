9cb330cd4c2139fc296ab565bf7971751c602b5e
Dionte Driskell <driskell06@gmail.com>
2024-04-22 07:57:37 -0400

    sort by age

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function sumArrNums(arr) {
     return count;
 }
 
-/** ternary operator is doing nothing when the element is a string, you can use Number (or +) to convert elements to numbers (strings will be converted, and numbers will remain numbers, so there is no need for type checking */
+/**  operator is doing nothing when the element is a string, you can use Number (or +) to convert elements to numbers (strings will be converted, and numbers will remain numbers, so there is no need for type checking */
 
 // Function to calculate the average of numbers in an array
 function averageArray(arr) {
@@ -29,15 +29,25 @@ function averageArray(arr) {
     // Dividing the sum by the length of the array to get the average
     const average = sum / arr.length;
     return average;
-  }
-  
-  //to print n
-  function printN {
+}
+
+//to print n
+function print(n) {
     if (n === 1) {
-        console.log (`n`);
+        console.log(`n`);
         return
     }
-     //recursive process
-  printN (n-1);
-  }
- 
+    //recursive process
+    print (n - 1);
+    console.log(print(n));
+}
+
+//   part 2 
+const arr[{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+    { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+    { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+    { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+    { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
+
+//sort the array by age
+sort(function (a, b) { return a.age - b.age; });
