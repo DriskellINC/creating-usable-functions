34a2bdb6ccd649afd846a5a8fe56bfdf9787fa55
Dionte Driskell <driskell06@gmail.com>
2024-04-22 05:52:48 -0400

    longeststring

---
+++ script.js
@@ -0,0 +1,22 @@
+// function sayHello () {
+//     console.log(`Hello!`);
+// }
+// sayHello() //function declaration
+
+// function expression
+// const sayHola = function (name) {
+//     console.log ("hola" + name + "!");
+// }
+// sayHola("Ishiah") // notes
+
+const sum = [sumNumber 
+ avg = (sumNumber/avgStringsLongerThan)
+
+const stringsLongerThan(['say', 'hello', 'in', 'the', 'morning']
+
+function sumArrNums(arr) {
+    let count = 0;
+    arr.forEach(el => typeof el == '' ? count+= el : '');
+    return count;
+}
+console.log(",, 3");
