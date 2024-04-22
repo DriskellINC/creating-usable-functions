25aa170d2d8ca720cf7873c5ada5fbfaf917a670
Dionte Driskell <driskell06@gmail.com>
2024-04-22 06:03:22 -0400

    recursive process

--- script.js
+++ script.js
@@ -16,7 +16,28 @@ const stringsLongerThan(['say', 'hello', 'in', 'the', 'morning']
 
 function sumArrNums(arr) {
     let count = 0;
-    arr.forEach(el => typeof el == '' ? count+= el : '');
+    arr.forEach(number => count += number());
     return count;
 }
-console.log(",, 3");
+
+/** ternary operator is doing nothing when the element is a string, you can use Number (or +) to convert elements to numbers (strings will be converted, and numbers will remain numbers, so there is no need for type checking */
+
+// Function to calculate the average of numbers in an array
+function averageArray(arr) {
+
+    const sum = sumArray(arr);      //sum of the array
+    // Dividing the sum by the length of the array to get the average
+    const average = sum / arr.length;
+    return average;
+  }
+  
+  //to print n
+  function printN {
+    if (n === 1) {
+        console.log (`n`);
+        return
+    }
+     //recursive process
+  printN (n-1);
+  }
+ 
